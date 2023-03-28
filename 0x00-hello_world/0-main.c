a4e867640931ba69a844dbf2bc3e8162847e93e2
Abdoul54 <abdelwahed.akhechane@gmail.com>
2023-03-28 15:09:09 -0700

    iii

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,3 +1,12 @@
+$ export CFILE=0-main.c ; ./0-preprocessor
+# 1 "0-main.c"
+# 1 "<built-in>" 1
+# 1 "<built-in>" 3
+# 399 "<built-in>" 3
+# 1 "<command line>" 1
+# 1 "<built-in>" 2
+# 1 "0-main.c" 2
+
 #include <stdio.h>
 
 /**
@@ -7,6 +16,6 @@
  */
 int main(void)
 {
-	printf("Coding is fun, but challenging!");
-	return (0);
+ printf("Hello, world!\n");
+ return (0);
 }
