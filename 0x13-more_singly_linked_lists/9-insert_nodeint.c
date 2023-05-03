c27be41012321e7114e690717c21c9b469493809
LO119 <93733505+Abdoul54@users.noreply.github.com>
2023-05-03 21:19:26 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position
- * @head: Pointer to the head node of the list
- * @idx: Index where the new node should be added (starting at 0)
- * @n: Integer value to be stored in the new node
+ * insert_nodeint_at_index - Inserts a new node at a given position in a list
+ * @head: Pointer to a pointer to the head of the list
+ * @idx: Index of the list where the new node should be added. Index starts at 0
+ * @n: Value to store in the new node
  *
  * Return: Address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
     listint_t *new_node, *temp;
-    unsigned int i = 0;
+    unsigned int i;
 
     if (head == NULL)
         return (NULL);
@@ -19,6 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return (NULL);
+
     new_node->n = n;
 
     if (idx == 0)
@@ -29,11 +30,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     }
 
     temp = *head;
-    while (temp != NULL && i < idx - 1)
-    {
+
+    for (i = 0; i < idx - 1 && temp != NULL; i++)
         temp = temp->next;
-        i++;
-    }
 
     if (temp == NULL)
     {
